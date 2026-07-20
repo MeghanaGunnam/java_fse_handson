8922c2edce0a45d44a98603fed1be9abcd04c5cf
MeghanaGunnam <meghana.evm@gmail.com>
2026-07-20 08:53:55 +0530

    Add Exercise 1 Spring and Maven

---
+++ Exercise 1 spring and maven/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+
+public class BookService {
+
+    public BookService() {
+        System.out.println("BookService Bean Created");
+    }
+
+    public void addBook() {
+        System.out.println("Book added successfully.");
+    }
+}
